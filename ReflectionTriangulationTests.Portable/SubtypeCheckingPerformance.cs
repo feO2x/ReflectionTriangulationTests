75ebf11da8fac3bbbd339f8cf1ca0a7ae06a7193
Kenny Pflug <kenny.pflug@live.de>
2016-08-24 17:47:27 +0200

    added tests indicating that the "is" operator is faster than typeof comparisons

--- ReflectionTriangulationTests.Portable/SubtypeCheckingPerformance.cs
+++ ReflectionTriangulationTests.Portable/SubtypeCheckingPerformance.cs
@@ -1,20 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
 using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace ReflectionTriangulationTests.Portable
 {
-    public class DowncastVsStringComparisonPerformanceTest
+    public sealed class SubtypeCheckingPerformance
     {
         private readonly ITestOutputHelper _output;
 
-        public DowncastVsStringComparisonPerformanceTest(ITestOutputHelper output)
+        public SubtypeCheckingPerformance(ITestOutputHelper output)
         {
             _output = output;
         }
@@ -25,7 +21,7 @@ namespace ReflectionTriangulationTests.Portable
             nameof(ConcreteType).Should().Be(typeof(ConcreteType).Name);
         }
 
-        [Fact(DisplayName = "Downcasts are faster than string comparisons")]
+        [Fact(DisplayName = "Downcasts are faster than string comparisons.")]
         public void DowncastVsStringComparison()
         {
             Abstraction instance = new ConcreteType();
@@ -34,7 +30,7 @@ namespace ReflectionTriangulationTests.Portable
             var downcastDuration = MeasureDowncastPerformance(instance);
 
             _output.WriteLine($"Downcast duration:          {downcastDuration.TotalMilliseconds:N}ms");
-            _output.WriteLine($"String comparison duration: {stringComparisonDuration.TotalMilliseconds}ms");
+            _output.WriteLine($"String comparison duration: {stringComparisonDuration.TotalMilliseconds:N}ms");
             stringComparisonDuration.Should().BeGreaterThan(downcastDuration);
         }
 
@@ -44,6 +40,7 @@ namespace ReflectionTriangulationTests.Portable
             stopwatch.Start();
             for (long i = 0; i < 100000000; ++i)
             {
+                // ReSharper disable once UnusedVariable
                 var foo = instance.TypeName == nameof(ConcreteType);
             }
             stopwatch.Stop();
@@ -57,7 +54,35 @@ namespace ReflectionTriangulationTests.Portable
             stopwatch.Start();
             for (long i = 0; i < 100000000; ++i)
             {
+                // ReSharper disable once UnusedVariable
                 var foo = instance is ConcreteType;
+            }
+            stopwatch.Stop();
+
+            return stopwatch.Elapsed;
+        }
+
+        [Fact(DisplayName = "Downcasts are faster than typeof comparisons.")]
+        public void DowncastVsTypeofComparison()
+        {
+            Abstraction instance = new ConcreteType();
+
+            var downcastDuration = MeasureDowncastPerformance(instance);
+            var typeofDuration = MeasureTypeofPerformance(instance);
+
+            _output.WriteLine($"Downcast duration:          {downcastDuration.TotalMilliseconds:N}ms");
+            _output.WriteLine($"typeof duration: {typeofDuration.TotalMilliseconds:N}ms");
+            typeofDuration.Should().BeGreaterThan(downcastDuration);
+        }
+
+        private static TimeSpan MeasureTypeofPerformance(Abstraction instance)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (long i = 0; i < 100000000; ++i)
+            {
+                // ReSharper disable once UnusedVariable
+                var foo = instance.GetType() == typeof(ConcreteType);
             }
             stopwatch.Stop();
 
@@ -67,6 +92,7 @@ namespace ReflectionTriangulationTests.Portable
         public abstract class Abstraction
         {
             public readonly string TypeName;
+
             protected Abstraction()
             {
                 TypeName = GetType().Name;
